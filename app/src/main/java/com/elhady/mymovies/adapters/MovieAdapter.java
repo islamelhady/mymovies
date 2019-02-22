627801f5ed5dfe7c43c1b275a14571235012a4eb
islamelhady <i.elhady44@gmail.com>
2019-02-22 02:30:48 +0200

    ADD model movie

---
+++ app/src/main/java/com/elhady/mymovies/adapters/MovieAdapter.java
@@ -0,0 +1,60 @@
+package com.elhady.mymovies.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.elhady.mymovies.R;
+import com.elhady.mymovies.models.Movies;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.MovieHolder> {
+    private List<Movies> moviesList;
+
+    public MovieAdapter(List<Movies> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @NonNull
+    @Override
+    public MovieHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View row = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.movie_row,viewGroup, false);
+        MovieHolder holder = new MovieHolder(row);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieHolder movieHolder, int position) {
+        Movies movie = moviesList.get(position);
+        movieHolder.nameMov.setText(movie.getMovieName());
+        movieHolder.rateMov.setText(movie.getMovieRate());
+        movieHolder.timeMov.setText(movie.getMovieTime());
+        movieHolder.storyMov.setText(movie.getMovieStory());
+        movieHolder.picMov.setImageResource(movie.getMoviePic());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+
+    public class MovieHolder extends RecyclerView.ViewHolder{
+        public TextView nameMov,rateMov,timeMov,storyMov;
+        public ImageView picMov;
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+            nameMov  = itemView.findViewById(R.id.movie_name);
+            rateMov  = itemView.findViewById(R.id.movie_rate);
+            timeMov  = itemView.findViewById(R.id.movie_time);
+            storyMov = itemView.findViewById(R.id.movie_story);
+            picMov   = itemView.findViewById(R.id.movie_pic);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/elhady/mymovies/models/Movies.java
@@ -0,0 +1,54 @@
+package com.elhady.mymovies.models;
+
+public class Movies {
+    private String movieName,movieRate,movieStory,movieTime;
+    private int moviePic;
+
+    public Movies(String movieName, String movieRate, String movieStory, String movieTime, int moviePic) {
+        this.movieName = movieName;
+        this.movieRate = movieRate;
+        this.movieStory = movieStory;
+        this.movieTime = movieTime;
+        this.moviePic = moviePic;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieRate() {
+        return movieRate;
+    }
+
+    public void setMovieRate(String movieRate) {
+        this.movieRate = movieRate;
+    }
+
+    public String getMovieStory() {
+        return movieStory;
+    }
+
+    public void setMovieStory(String movieStory) {
+        this.movieStory = movieStory;
+    }
+
+    public String getMovieTime() {
+        return movieTime;
+    }
+
+    public void setMovieTime(String movieTime) {
+        this.movieTime = movieTime;
+    }
+
+    public int getMoviePic() {
+        return moviePic;
+    }
+
+    public void setMoviePic(int moviePic) {
+        this.moviePic = moviePic;
+    }
+}
