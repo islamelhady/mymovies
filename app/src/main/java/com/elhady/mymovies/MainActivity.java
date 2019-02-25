b2e42488f1f86e17f79feb2a81d003c984656af2
islamelhady <i.elhady44@gmail.com>
2019-02-26 00:43:52 +0200

    ADD all data to string xml

--- app/src/main/java/com/elhady/mymovies/MainActivity.java
+++ app/src/main/java/com/elhady/mymovies/MainActivity.java
@@ -28,27 +28,14 @@ public class MainActivity extends AppCompatActivity {
                          R.drawable.badsamaritan,R.drawable.blockers,R.drawable.ifeel_pretty,
                          R.drawable.isleof_dogs,R.drawable.love_simon,R.drawable.rampage};
 
-        String movName[] = {"A Quiet Place","Avengers: Infinity War","Bad Samaritan","Blockers","I Feel Pretty","Isle of Dogs","Love, Simon","Rampage"};
+        //int movPic[] = getResources().getIntArray(R.array.movie_pics);
+        String movName[] = getResources().getStringArray(R.array.movie_name);
 
-        String movRate[] = {"6.1","8.2","7.9","8.1","6.7","4.5","5.6","8.6"};
+        String movRate[] = getResources().getStringArray(R.array.movie_rate);
 
-        String movStory[] = {getResources().getString(R.string.a_quiet_place),
-                             getResources().getString(R.string.avengers),
-                             getResources().getString(R.string.badsamaritan),
-                             getResources().getString(R.string.blockers),
-                             getResources().getString(R.string.ifeel_pretty),
-                             getResources().getString(R.string.isleof_dogs),
-                             getResources().getString(R.string.love_simon),
-                             getResources().getString(R.string.rampage)};
+        String movStory[] = getResources().getStringArray(R.array.movie_story);
 
-        String movTime[] = {getResources().getString(R.string.min_a_quiet_place),
-                            getResources().getString(R.string.min_avengers),
-                            getResources().getString(R.string.min_badsamaritan),
-                            getResources().getString(R.string.min_blockers),
-                            getResources().getString(R.string.min_ifeel_pretty),
-                            getResources().getString(R.string.min_isleof_dogs),
-                            getResources().getString(R.string.min_love_simon),
-                            getResources().getString(R.string.min_rampage)};
+        String movTime[] = getResources().getStringArray(R.array.movie_time);
 
         for (int i = 0; i < movPic.length; i++){
             Movies movies = new Movies(movName[i],movRate[i],movStory[i],movTime[i],movPic[i]);
